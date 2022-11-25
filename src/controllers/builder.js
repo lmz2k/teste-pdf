b082946ccb340ee09d899ca469b6094445bfda89
Gabriel Lima <glimasdev@gmail.com>
2022-11-25 11:53:19 -0300

    .

--- src/controllers/builder.js
+++ src/controllers/builder.js
@@ -1,16 +1,12 @@
 const builderService = require('../services/builder');
 
 async function build(req, res) {
-  try {
-    const { url } = req.body;
+  const { url } = req.body;
 
-    const pdf = await builderService.builder(url);
+  const pdf = await builderService.builder(url);
 
-    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
-    res.send(pdf);
-  } catch (e) {
-    res.json({ error: 'erro' });
-  }
+  res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
+  res.send(pdf);
 }
 
 module.exports = {
