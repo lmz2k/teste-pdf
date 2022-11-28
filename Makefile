d2dc791aae9fd06040ace0fe0a044d297118cb54
Gabriel Lima <glimasdev@gmail.com>
2022-11-27 21:36:07 -0300

    .

---
+++ Makefile
@@ -0,0 +1,8 @@
+up:
+	docker-compose up
+
+build:
+	docker-compose build --no-cache
+
+exec:
+	docker-compose exec pdf-builder bash
--- src/services/builder.js
+++ src/services/builder.js
@@ -1,16 +1,27 @@
-const puppeteer = require('puppeteer-core');
-const chromium = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer');
 
 async function builder(url) {
+  console.log('Starting PDF conversion of ', url);
+
+  // if is not using docker, the path is not necessary if system already has Google Chrome.
+  const path = process.env.isDocker ? '/usr/bin/google-chrome-stable' : undefined;
+
   const browser = await puppeteer.launch({
-    headless: chromium.headless,
-    executablePath: await chromium.executablePath,
+    executablePath: path,
+    ignoreDefaultArgs: ['--disable-extensions'],
+    args: [
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--use-gl=egl',
+      '--disable-gpu',
+      '--no-sandbox',
+    ],
+    headless: true,
   });
 
   const page = await browser.newPage();
 
   await page.emulateMediaType('screen');
-
   await page.goto(url, { waitUntil: 'networkidle0' });
 
   const pdf = await page.pdf({
@@ -19,6 +30,7 @@ async function builder(url) {
     height: 700,
   });
 
+  console.log('Ending PDF conversion of ', url);
   await browser.close();
   return pdf;
 }
